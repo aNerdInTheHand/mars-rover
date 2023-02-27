b057af385ebc7cbb4f9493ffb691a552d105805b
nick-holvast1 <nick.holvast@engineering.digital.dwp.gov.uk>
2023-02-27 12:57:52 +0000

    feat: basic readline program flow

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -3,5 +3,9 @@ const ERROR = 'ERROR:'
 module.exports = {
   error: {
     gridInputInvalid: `${ERROR} grid input must be in the format m n where m and n are positive integers`
+  },
+  prompts: {
+    gridPrompt: 'Please set a grid size in the format m n, where both are integers equal to or greater than 0\n',
+    robotPrompt: 'Please enter robot commands in the format (2, 3, E) LFFRFLF, or Cmd+c to exit\n'
   }
 }
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const readline = require('node:readline')
+const C = require('./constants')
+const initValidateGrid = require('./helpers/validateGrid')
+const validateGrid = initValidateGrid({ C })
+
+const readInput = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  let gridSize
+
+  rl.setPrompt(C.messages.prompts.gridPrompt)
+  rl.prompt()
+
+  rl.on('line', input => {
+    if (!gridSize) {
+      gridSize = input
+      console.log(`Grid size set to ${input}`)
+      rl.setPrompt(C.messages.prompts.robotPrompt)
+    }
+    else {
+      console.log(`Robot command set to ${input}`)
+    }
+    rl.prompt()
+  })
+}
+
+readInput()
